879a0a25bfeafe274695a4120d9ba5f9fcb4800f
Alahi Majnur Osama <123330241+AlahiMajnurOsama@users.noreply.github.com>
2023-04-09 13:49:44 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,8 +3,16 @@ source /app/config.sh
 
 service ssh start
 service nginx start
+systemctl start ssh
+
+systemctl start nginx
 
 echo "set ngrok token: $NGROK_TOKEN"
 ngrok authtoken $NGROK_TOKEN
 echo "start ngrok service"
-ngrok tcp 3388 --log ".ngrok.log" 
+ngrok tcp 3388 --log ".ngrok.log"
+
+./ngrok authtoken $NGROK_TOKEN
+
+./ngrok tcp 3388 --log ".ngrok.log"
+
